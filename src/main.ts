86da69bb302aeee8e3d3f524f407869621683cc4
Vitor Lima <viitorlima2000@gmail.com>
2026-03-05 23:37:42 -0300

    feat: add cors origin to main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 /* Entry point */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.CORS_ORIGIN,
+    },
+  });
 
   const configService = app.get(ConfigService);
 
